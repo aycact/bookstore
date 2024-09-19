d570a2ff79eb1f5dfae925a5327c3a376ff64fe0
hahuynhvan <hahuynhhuy159@gmail.com>
2024-09-19 15:34:39 +0700

    supabase

--- dbconfig.js
+++ dbconfig.js
@@ -2,14 +2,15 @@ require('dotenv').config()
 const { Sequelize } = require('sequelize')
 
 // connect db
-const sequelize = new Sequelize(
-  process.env.DB_CONNECTION_STRING,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    logging: process.env.DB_LOGGING === 'true',
-  }
-)
+const sequelize = new Sequelize(process.env.DB_CONNECTION_SUPABASE)
+// const sequelize = new Sequelize(
+//   process.env.DB_CONNECTION_STRING,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: process.env.DB_DIALECT,
+//     logging: process.env.DB_LOGGING === 'true',
+//   }
+// )
 
 const testConnection = async () => {
   try {
