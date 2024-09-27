21346fc19978dec434593d5a81e8ffaec85f6d10
aycact <quangtri.lam.9@gmail.com>
2024-09-27 14:43:14 +0700

    đẩy vô env

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+require("dotenv").config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
+      "/api": {
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
       },
     },
   },
   resolve: {
     alias: {
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
     },
   },
-})
+});
