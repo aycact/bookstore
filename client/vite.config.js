025f4b851c286777cf62c2316bf74973c0626499
aycact <quangtri.lam.9@gmail.com>
2024-10-05 08:13:48 +0700

    add lighning css

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import browserslist from 'browserslist'
+import { browserslistToTargets } from 'lightningcss'
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    lightningcss: {
+      targets: browserslistToTargets(browserslist('>= 0.25%')),
+    }
+  },
+  build: {
+    cssMinify: 'lightningcss'
+  },
   base: '/',
   plugins: [react()],
   resolve: {
