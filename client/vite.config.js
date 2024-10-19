1dace407de5d04a329bd907ff5b2fced3719a689
aycact <quangtri.lam.9@gmail.com>
2024-10-19 08:10:12 +0700

    added lightningcss and delete netlify

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import browserslist from 'browserslist';
+import {browserslistToTargets} from 'lightningcss';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
@@ -10,4 +12,12 @@ export default defineConfig({
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
     },
   },
+  css: {
+    lightningcss: {
+      targets: browserslistToTargets(browserslist('last 2 versions'))
+    }
+  },
+  build: {
+    cssMinify: 'lightningcss'
+  }
 })
